34d740861c7882cbdf128966873603ac97a1b30c
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2024-09-06 16:27:43 +0900

    Remove whitespaces

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use regex::Regex;
 fn main() {
     // Get the user's home directory
     let home_dir = env::var("HOME").unwrap();
-    
+
     // Regex to match escape sequences
     let escape_seq_re = Regex::new(r"\x1B\[[0-9;]*m$").unwrap();
-    
+
     // Read from stdin
     let stdin = io::stdin();
     let handle = stdin.lock();
-    
+
     for line in handle.lines() {
         match line {
             Ok(mut line) => {
